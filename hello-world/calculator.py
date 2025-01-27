e5e915048bab02e620fa8502541e8c2f8e90167e
Gavin-Ricks <gavin.ricks2020@gmail.com>
2025-01-27 06:38:29 +0000

    Some CS50 Stuff Alongside w3 schoools

---
+++ hello-world/calculator.py
@@ -0,0 +1,6 @@
+x = float(input("Whats x? "))
+y = float(input("What's y? "))
+
+z = round(x/y,2)
+
+print(z)
---
+++ hello-world/hello.py
@@ -0,0 +1,9 @@
+#Names + Strip + Title Case it in one line!
+name = input("What's your name? ").strip().title()
+
+
+#Print types
+# print("Hello!", name)
+# print("Hello!" + name)
+# print("Hello, \"Friend\"")
+print(f"Hello, {name}")
